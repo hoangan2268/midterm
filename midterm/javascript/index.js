bc98894b3159bee72f95b73315a6ae0824509a77
hoangan2268 <92203264+hoangan2268@users.noreply.github.com>
2021-11-14 16:25:02 +0700

    Add files via upload

---
+++ midterm/javascript/index.js
@@ -0,0 +1,62 @@
+// V. Javascript
+1.
+// let people= prompt('How many People are there?')
+// let pizza= prompt('How many Pizza(s) are there?')
+// let slices= Number(pizza) * (8)
+// let average= Number(slices) / Number (people)
+// alert ('There are ' + people + ' People and ' + pizza + ' Pizzas. Total slices are ' + slices )
+// alert ('Each Person can have an average of ' + average + ' Pizza slice(s). ')
+// console.log('People: ' + people)
+// console.log('Pizza(s): ' + pizza)
+// console.log('Slices: ' + slices)
+// console.log('Average: ' + average)
+
+2.
+// let temp=prompt('What is the temperature today?')
+// if(temp > 40){
+//     alert('Dangerous weather!')
+// }else if(temp > 25 ){
+//     alert('The weather is hot!')
+// }else if(temp > 15){
+//     alert('The weather is nice!')
+// }else if(temp >0){
+//     alert('The weather is cold!')
+// }else if(temp >-12){
+//     alert('The weather is super cold!')
+// }else if(temp < -12){
+//     alert('Dangerous weather!')
+// }
+
+3.
+// for(let i=0; i < 10; i++){
+//     let username=prompt('What is your name?');
+// }   
+
+4.
+// function sumCalculator(x, y){
+//     var total= x + y;
+//     console.log(total)
+// }
+// sumCalculator(9, 10);
+
+5.
+// let student= {
+//     name:"David",
+//     job: "Coder",
+//     address: "America",
+//     age: "40",
+// }
+
+6.
+// const list = ["Jeans", "T-shirt", "Short"];
+
+7.
+//const newItem= 'Dress';
+// list.push(newItem);
+
+// const itemIndex= 1;
+// const newItem = 'T-shirt (Updated)';
+// list[itemIndex]=newItem;
+
+// const itemIndex= 1;
+// list.splice(itemIndex, 1)
